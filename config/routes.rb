ce3857e04351952bea3a198bd78b1f9358e3b94e
Masato Ito <masarusan24@hotmail.com>
2018-01-16 03:03:48 +0000

    Micropost Model,Microposts Controller作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'microposts/new'
+
+  get 'microposts/edit'
+
+  get 'microposts/index'
+
+  get 'microposts/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
